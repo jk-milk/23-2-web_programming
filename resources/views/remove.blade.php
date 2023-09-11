90c3b5a95173f2a81ba6b70fb562d2f457ba8cf9
jk <yjkeong@gmail.com>
2023-09-11 20:47:15 +0900

    remove_form 추가 UserController 껍데기 추가

---
+++ resources/views/remove.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=kr>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>remove</title>
+
+    </head>
+    <body>
+      <h1>요청 처리결과</h1>
+      <h2>탈퇴된 회원 : {{$name}}</h2>
+
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,34 +16,51 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::get('/test', function(){
-    return view('welcome');
+	return view('welcome');
 }); // 클로저
 
 Route::get('/register', function(){
-    return view('register_form');
+	return view('register_form');
 });
 
 Route::post('/register', function(Request $req){
-    // $email = $req->input("email"); $email = $req->email;
-    $name = $req->input("name");
-    $email = $req->input("email");
-    $birthDate = $req->input("birthDate");
-    $organization = $req->input("organization");
-    return view('register', ['name'=>$name, 'email'=>$email, 'birthDate'=>$birthDate, 'organization'=>$organization]);
+	// $email = $req->input("email"); $email = $req->email;
+	$name = $req->input("name");
+	$email = $req->input("email");
+	$birthDate = $req->input("birthDate");
+	$organization = $req->input("organization");
+	return view('register', ['name'=>$name, 'email'=>$email, 'birthDate'=>$birthDate, 'organization'=>$organization]);
 });
 
 Route::get('/update', function(){
-    return view('update_form');
+	return view('update_form');
 });
 
 Route::put('/update', function(Request $req){
-    $name = $req->input("name");
-    $email = $req->input("email");
-    $birthDate = $req->input("birthDate");
-    $organization = $req->input("organization");
-    return view('update', ['name'=>$name, 'email'=>$email, 'birthDate'=>$birthDate, 'organization'=>$organization]);
+	$name = $req->input("name");
+	$email = $req->input("email");
+	$birthDate = $req->input("birthDate");
+	$organization = $req->input("organization");
+	return view('update', ['name'=>$name, 'email'=>$email, 'birthDate'=>$birthDate, 'organization'=>$organization]);
+});
+
+Route::get('/remove', function(){
+	return view('remove_form');
+});
+
+Route::delete('/remove', function(Request $req){
+	$name = $req->input("name");
+	return view('remove', ['name'=>$name]);
+});
+
+Route::get('/user/{id?}', function(string $id = '100'){
+	return 'User '.$id;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function(string $postId, string $commentId) {
+	return '게시글 '.$postId.'번 글의 댓글 '.$commentId.'번을 인출했습니다.';
 });
