db8c91bca96961c0c23f95bce8bc35a4be4e162a
jk <yjkeong@gmail.com>
2023-09-14 20:49:23 +0900

    controller 종료 db 없이 redirect 맛보기

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\UserController as ControllersUserController;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+	protected static $users =  [['id'=>1, 'name'=>'고길동', 'birthDate'=>'1999/01/02', 'email'=>'gdo@gmail.com'],
+															['id'=>2, 'name'=>'홍길동', 'birthDate'=>'1998/01/02', 'email'=>'gdh@gmail.com'], 
+															['id'=>3, 'name'=>'박동훈', 'birthDate'=>'1599/05/02', 'email'=>'dhp@gmail.com'], 
+															['id'=>4, 'name'=>'박찬호', 'birthDate'=>'1699/02/02', 'email'=>'chp@gmail.com'], 
+															['id'=>5, 'name'=>'박문수', 'birthDate'=>'1990/07/02', 'email'=>'msp@gmail.com'],
+														]; 
 	/**
 	 * Display a listing of the resource.
 	 */
@@ -17,13 +24,8 @@ class UserController extends Controller
 			2. 가져온 사용자 정보를 blade 파일에 넘겨주면서 실행한다.
 		*/
 		// DB에서 읽어온 정보를 $users 변수에 할당했다고 가정.
-		$users = [['id'=>1, 'name'=>'고길동', 'birthDate'=>'1999/01/02', 'email'=>'gdgo@gmail.com'],
-							['id'=>2, 'name'=>'홍길동', 'birthDate'=>'1998/01/02', 'email'=>'gdh@gmail.com'], 
-							['id'=>3, 'name'=>'박동훈', 'birthDate'=>'1599/05/02', 'email'=>'dhp@gmail.com'], 
-							['id'=>4, 'name'=>'박찬호', 'birthDate'=>'1699/02/02', 'email'=>'chp@gmail.com'], 
-							['id'=>5, 'name'=>'박문수', 'birthDate'=>'1990/07/02', 'email'=>'msp@gmail.com'],
-						]; 
-		return view('welcome', ['users'=>$users]);
+		
+		return view('welcome', ['users'=>UserController::$users]);
 		
 	}
 
@@ -53,6 +55,7 @@ class UserController extends Controller
 		$org 				= $request->input("org");
 
 		return view('register', ['name'=>$name, 'birthDate'=>$birthDate, 'email'=>$email, 'org'=>$org]);
+		//return redirect()
 	}
 
 	/**
@@ -63,9 +66,22 @@ class UserController extends Controller
 		// 상세 보기
 		/*
 			1. $id를 가지고 DB에서 레코드 하나를 인출한다.
+				// select * from users where id = $id
 			2. 인출된 그 정보를 변수 $user에 할당
 			3. 그 $user 값의 blade에 전달하면서 실행. 
 		*/
+		$userFound = null;
+		foreach (UserController::$users as $user) {
+			if ($user["id"] == $id) {
+				$userFound = $user;
+				break;
+			}
+		}
+		// $userFound : ['id'=>1, 'name'=>'고길동', ...]
+		// 못 찾은 경우 -> $userFound는 null
+		// 이 때 null 대신에 빈 배열 []를 블레이드 파일에 넘겨주기
+		$userFound = $userFound!=null ? $userFound : [];
+		return view('user_info', ['user'=>$userFound]);
 	}
 
 	/**
@@ -74,6 +90,19 @@ class UserController extends Controller
 	public function edit(string $id)
 	{
 		// 수정 폼
+		/*
+			1. $id 값에 해당하는 사용자 정보를 DB에서 읽어온다.
+			2. 읽어온 그 사용자 정보를 blade 파일에 넘겨주면서 그 blade를 실행.
+		*/
+		$userFound = null;
+		foreach (UserController::$users as $user) {
+			if ($user["id"] == $id) {
+				$userFound = $user;
+				break;
+			}
+		}
+		// $userFound : ['id'=>1, 'name'=>'고길동', ...]
+		return view('update_form', ['user'=>$userFound]);
 	}
 
 	/**
@@ -82,6 +111,29 @@ class UserController extends Controller
 	public function update(Request $request, string $id)
 	{
 		// 수정하기
+		/*
+			1. Request 객체에서 사용자가 입력한 값을 빼와야 한다.
+			2. 위에서 빼온 값으로 $id에 해당하는 DB 레코드를 찾아서 update를 한다.
+			3. 사용자 상세보기 view로 연결시켜 준다.
+		*/
+		$name = $request->input("name");
+		$birthDate = $request->input("birthDate");
+		$email = $request->input("email");
+		$updateUser = null;
+
+		foreach (UserController::$users as $user) {
+			if ($user["id"] == $id) {
+				$user["name"] = $name;
+				$user["birthDate"] = $birthDate;
+				$user["email"] = $email;
+				$updateUser = $user;
+				break;
+			}
+		}
+
+		//return view('user_info', ['user'=>$updateUser]);
+
+		return redirect('/users/'.$updateUser['id']);
 	}
 
 	/**
@@ -90,5 +142,16 @@ class UserController extends Controller
 	public function destroy(string $id)
 	{
 		// 삭제하기
+		/*
+			1. PRIMARY KEY 칼럼 값으로 $id 값을 가지는 레코드를 DB에서 찾아서 삭제
+			2. 리스트 페이지를 생성해 반환
+		*/
+		for ($i = 0; $i < sizeof(UserController::$users); $i++) {
+			if (UserController::$users[$i]['id'] == $id) {
+				unset(UserController::$users[$i]);
+			}
+		}
+		// return view('welcome', ['users'=>UserController::$users]);
+		return redirect('/users');
 	}
 }
--- resources/views/update_form.blade.php
+++ resources/views/update_form.blade.php
@@ -8,26 +8,13 @@
 
     </head>
     <body>
-      <form action="/update" method="POST">
+      <form action="/users/{{$user['id']}}" method="POST">
         @method('PUT')
         @csrf
-        <div>
-          <label for="name">이름:</label>
-          <input type="text" name="name" value="고길동" readonly/>
-        </div>
-        <div>
-          <label for="birthDate">생년월일(YYYY/MM/DD):</label>
-          <input type="text" name="birthDate" value="1980/02/25" readonly/>
-        </div>
-        <div>
-          <label for="email">email:</label>
-          <input type="email" name="email" value="gdgd@yju.ac.kr" />
-        </div>
-        <div>
-          <label for="organization">소속:</label>
-          <input type="organization" name="organization" value="MBC"/>
-        </div>
-        <input type="submit" value="수정">
+        이름 : <input type="text" name="name" value='{{$user["name"]}}'> <br>
+        이메일 : <input type="text" name="email" value='{{$user["email"]}}'> <br>
+        생년월일 : <input type="text" name="birthDate" value='{{$user["birthDate"]}}'> <br>
+        <input type="submit" value="확인">
       </form>
     </body>
 </html>
