3a929d7edcd1d513d67b6fedb511917cbadce443
jk <yjkeong@gmail.com>
2023-11-07 14:08:45 +0900

    lastetOfMany, oldestOfMany, hasManyThrough

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,23 @@ class User extends Authenticatable
     public function posts() {
         return $this->hasMany(Post::class);
     }
+
+    public function latestPost() {
+        // 이 user가 작성한 posts 중에 id 값이 가장 큰 것
+        return $this->hasOne(Post::class)->latestOfMany();
+    }
+
+    public function oldestPost() {
+        // 이 user가 작성한 posts 중에 id 값이 가장 작은 것
+        return $this->hasOne(Post::class)->oldestOfMany();
+    }
+
+    // 내가 작성한 게시글의 모든 댓글 가져오기
+    // users --- posts --- comments
+    //     user_id    post_id, id, id
+    public function postcomments() {
+        // return $this->hasManyThrough(Comment::class, Post::class);
+        return $this->hasManyThrough(Comment::class, Post::class, 'user_id', 'post_id', 'id', 'id');
+
+    }
 }
